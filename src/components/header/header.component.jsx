187b4af5745d8923ecc1d1e1c500676acc3a5acd
Abay Ibrayev <abayibrayev7@gmail.com>
2021-09-19 19:18:53 +0100

    header implemented

--- src/components/header/header.component.jsx
+++ src/components/header/header.component.jsx
@@ -1,17 +1,76 @@
-import React from 'react';
-import { Link } from 'react-router-dom';
+import React, { useState } from 'react';
+import { NavLink } from 'react-router-dom';
+
+import './header.styles.scss';
 
 const Header = () => {
+  const [menu, setMenu] = useState(false);
+
   return (
-    <div>
-      <Link to='/dashboard'>Dashboard</Link>
-      <Link to='/agency'>Our Agency</Link>
-      <Link to='/'>Our Staff</Link>
-      <Link to='/locations'>Locations</Link>
-      <Link to='/latest'>Latest</Link>
-      <Link to='/upcoming'>Upcoming</Link>
-      <Link to='/resources'>Resources</Link>
-      <Link to='/your'>Your Stein IAS</Link>
+    <div className={`header ${menu && 'header__open'}`}>
+      <div className='links'>
+        <NavLink
+          to='/dashboard'
+          activeClassName='active'
+          onClick={() => setMenu(false)}
+        >
+          Dashboard
+        </NavLink>
+        <NavLink
+          to='/agency'
+          activeClassName='active'
+          onClick={() => setMenu(false)}
+        >
+          Our Agency
+        </NavLink>
+        <NavLink
+          exact
+          to='/'
+          activeClassName='active'
+          onClick={() => setMenu(false)}
+        >
+          Our Staff
+        </NavLink>
+        <NavLink
+          to='/locations'
+          activeClassName='active'
+          onClick={() => setMenu(false)}
+        >
+          Locations
+        </NavLink>
+        <NavLink
+          to='/latest'
+          activeClassName='active'
+          onClick={() => setMenu(false)}
+        >
+          Latest
+        </NavLink>
+        <NavLink
+          to='/upcoming'
+          activeClassName='active'
+          onClick={() => setMenu(false)}
+        >
+          Upcoming
+        </NavLink>
+        <NavLink
+          to='/resources'
+          activeClassName='active'
+          onClick={() => setMenu(false)}
+        >
+          Resources
+        </NavLink>
+        <NavLink
+          to='/your'
+          activeClassName='active'
+          onClick={() => setMenu(false)}
+        >
+          Your Stein IAS
+        </NavLink>
+      </div>
+
+      <button className='hamburgerBtn' onClick={() => setMenu(!menu)}>
+        <i className='fa fa-bars'></i>
+      </button>
     </div>
   );
 };
